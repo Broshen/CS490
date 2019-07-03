a2427609e6a7241132d6bafd9b9917efc6ac05d9
melen <hweigao@hotmail.com>
2019-07-03 17:12:41 -0400

    added consultant info to cards

--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -6,28 +6,29 @@ import {Card, Button} from 'react-bootstrap';
 
 class CFCCard extends Component {
 
+
   render() {
     return (
       <Card className="card">
         <div className="row">
           <div className="card-thumbnail">
-            <img src="http://fill-image.jobs160.com/300x200"/>
+            <img src={this.props.consultant.thumbnail? this.props.consultant.thumbnail: "http://fill-image.jobs160.com/300x200"}/>
           </div>
           <div className="card-content-wrapper">
             <div className="card-content">
               <span className="row">
                 <div className="card-content-info">
-                  <div className="card-content-info-title">SENIOR CONSULTANT</div>
-                  <div className="card-content-info-name">Joe Conner</div>
+                  <div className="card-content-info-title">{this.props.consultant.position? this.props.consultant.position: "SENIOR CONSULTANT"}</div>
+                  <div className="card-content-info-name">{this.props.consultant.name}</div>
                   <div className="card-content-info-details">
-                    17 Previous Projects · No Relocation Required
+                    17 Previous Projects{this.props.consultant.locations.map((location, i) => {return " · "+location})}
                   </div>
                   <div className="card-content-info-relevance">
-                    4 Relevant Experiences
+                    <span>Rating: </span> {this.props.consultant.avg_rating}/10
                   </div>
                 </div>
                 <div className="card-content-rate">
-                  <span>$150/Day</span>
+                  <span>${this.props.consultant.pay_amount}/{this.props.consultant.pay_frequency}</span>
                 </div>
               </span>
             </div>
